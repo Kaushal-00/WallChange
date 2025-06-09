5787643792ebe22d330bc32d1efb6c716d4d8703
Kaushal Kathiriya <kaushalkathiriya2005@gmail.com>
2025-06-09 23:02:15 +0530

    Update a main function.

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         ConfigManager config = new ConfigManager();
-        WallpaperChanger changer = new WallpaperChanger();
+        WallManager changer = new WallManager();
 
         config.loadOrCreateConfig();
         changer.loadImages(config.getWallpaperFolder());

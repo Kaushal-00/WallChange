a2810a53c051b651eceee70567ce1c2026826b18
Kaushal Kathiriya <kaushalkathiriya2005@gmail.com>
2025-06-09 22:59:47 +0530

    Add WallManager to load images and set next wallpaper.

---
+++ WallManager.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class WallManager {
+
+    private ArrayList<String> imageList = new ArrayList<>();
+
+    public void loadImages(String folderPath) {
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+
+        if (files != null) {
+            for (File f : files) {
+                if (f.isFile() && isImage(f.getName())) {
+                    imageList.add(f.getName());
+                }
+            }
+        }
+    }
+
+    public void setNextWallpaper(ConfigManager config) throws IOException {
+        int index = config.getCurrentIndex();
+        if (imageList.isEmpty()) {
+            System.out.println("No images found in folder.");
+            return;
+        }
+
+        String nextImage = imageList.get(index);
+        String fullPath = config.getWallpaperFolder() + "\\" + nextImage;
+
+        String command = "powershell -command \"Add-Type -TypeDefinition \\\"using System.Runtime.InteropServices; " +
+                "public class Wallpaper { [DllImport(\\\"user32.dll\\\")] public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }\\\"; " +
+                "[Wallpaper]::SystemParametersInfo(20, 0, '" + fullPath + "', 3)\"";
+
+        Runtime.getRuntime().exec(command);
+
+        System.out.println("Wallpaper changed to: " + nextImage);
+
+        int newIndex = (index + 1) % imageList.size();
+        config.updateIndex(newIndex);
+    }
+
+    private boolean isImage(String name) {
+        name = name.toLowerCase();
+        return name.endsWith(".jpg") || name.endsWith(".jpeg") || name.endsWith(".png") || name.endsWith(".bmp");
+    }
+}
